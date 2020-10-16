4ed105b754ecd2a0f27d05dfa4f6331076bb241b
chacha86 <taejincha0619@gmail.com>
2020-10-16 12:59:15 +0900

    명령어 입력 기능 구현

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,36 @@
+import java.util.Scanner;
 
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("start~!");
+		
+		Scanner sc = new Scanner(System.in);
+		String title = "";
+		String body = "";
+		
+		while(true) {
+			System.out.print("명령어 입력 : ");
+			String cmd = sc.next();
+			if(cmd.equals("exit")) {
+				System.out.println("종료");
+				break;
+			}
+			if(cmd.equals("add")) {
+				
+				System.out.println("게시물 제목을 입력해주세요 :");
+				title = sc.next();
+				System.out.println("게시물 내용을 입력해주세요 :");
+				body = sc.next();
+				System.out.println("게시물이 등록되었습니다.");
+			}
+			if(cmd.equals("list")) {
+				System.out.println("제목 : " + title);
+				System.out.println("내용 : " + body);
+			}
+			
+		}
+		
+		
 	}
 
 }
