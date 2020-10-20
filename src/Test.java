a9f7ccd87971e52681b24ea8b112402a0a4c7e07
chacha86 <taejincha0619@gmail.com>
2020-10-20 11:35:19 +0900

    d4

--- src/Test.java
+++ src/Test.java
@@ -6,9 +6,11 @@ public class Test {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
+		ArrayList<Integer> ids = new ArrayList<>();
 		ArrayList<String> titles = new ArrayList<>();
 		ArrayList<String> bodies = new ArrayList<>();
 		int size = 0;
+		int no = 1;
 
 		while (true) {
 			System.out.print("명령어 입력 : ");
@@ -18,7 +20,8 @@ public class Test {
 				break;
 			}
 			if (cmd.equals("add")) {
-
+				ids.add(no);
+				no++;
 				System.out.println("게시물 제목을 입력해주세요 :");
 				String title = sc.next();
 				titles.add(title);
@@ -30,6 +33,7 @@ public class Test {
 			}
 			if (cmd.equals("list")) {
 				for (int i = 0; i < size; i++) {
+					System.out.println("번호 : " + ids.get(i));
 					System.out.println("제목 : " + titles.get(i));
 					System.out.println("내용 : " + bodies.get(i));
 					System.out.println("===================");
@@ -39,11 +43,11 @@ public class Test {
 			if(cmd.equals("update")) {
 				
 				System.out.println("수정할 게시물 선택 : ");
-				String targetTitle = sc.next();
+				int targetId = sc.nextInt();
 				
-				for(int i = 0; i < titles.size(); i++) {
-					String title = titles.get(i);
-					if(title.equals(targetTitle)) {
+				for(int i = 0; i < ids.size(); i++) {
+					int id = ids.get(i);
+					if(id == targetId) {
 						System.out.println("게시물 제목을 입력해주세요 :");
 						String newTitle = sc.next();
 						
