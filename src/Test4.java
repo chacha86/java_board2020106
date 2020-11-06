14b4219f6740fa3703b2ff63f4fbd5bfcd6a588b
jhs512 <jangka512@gmail.com>
2020-11-06 13:02:37 +0900

    페이징1

---
+++ src/Test4.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Test4 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int currentPageNo = 12; // 현재 페이지
+		int totalCntOfItems = 20; // 전체 게시물 개수
+		int startPageNo = 1; // 시작 페이지 번호
+		int itemsCntPerPage = 3; // 페이지당 출력 게시물 개수
+		int pageCntPerBlock = 5; // 한 페이지 블록 당 페이지 개수
+		int endPageNo = (int)Math.ceil((double)totalCntOfItems / itemsCntPerPage); // 마지막 페이지 번호
+		int currentPageBlock = (int)Math.ceil((double)currentPageNo / pageCntPerBlock) ; // 현재 페이지 블록
+		
+		int startPageNoInBlock = (currentPageBlock - 1) * pageCntPerBlock + 1 ; // 현재 페이지 블록의 시작 페이지 번호
+		int endPageNoInBlock = startPageNoInBlock + pageCntPerBlock - 1;// // 현재 페이지 블록의 마지막 페이지 번호
+		
+		for(int i = startPageNoInBlock ; i <= endPageNoInBlock; i++ ) {
+			
+			if(i == currentPageNo) {
+				System.out.print("[" + i+ "] ");
+			} else {
+				System.out.print(i+ " ");
+			}
+		}
+	}
+
+}
